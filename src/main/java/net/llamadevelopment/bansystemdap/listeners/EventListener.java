e646a503705616f4f46069280ebe762f48ba0793
Mundschutziii <jop.pretzer@gmx.de>
2020-12-29 17:08:21 +0100

    Update EventListener.java

--- src/main/java/net/llamadevelopment/bansystemdap/listeners/EventListener.java
+++ src/main/java/net/llamadevelopment/bansystemdap/listeners/EventListener.java
@@ -3,7 +3,6 @@ package net.llamadevelopment.bansystemdap.listeners;
 import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
-import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.player.PlayerPreLoginEvent;
 import lombok.AllArgsConstructor;
 import net.llamadevelopment.bansystem.BanSystem;
@@ -19,11 +18,6 @@ public class EventListener implements Listener {
     public void on(final PlayerPreLoginEvent event) {
         final Player player = event.getPlayer();
         this.instance.provider.updatePlayerIp(player.getName(), player.getSocketAddress());
-    }
-
-    @EventHandler
-    public void on(final PlayerJoinEvent event) {
-        final Player player = event.getPlayer();
         final Provider api = BanSystem.getApi().getProvider();
         this.instance.provider.getDuplicateAccounts(player.getName(), accounts -> {
             accounts.remove(event.getPlayer().getName());
